a52801d89316f62eab5834d0144c8d7edd58923d
Oleksii-dot278 <obukrieiev2023@itcollege.lviv.ua>
2025-11-22 16:54:45 +0100

    Updated

--- code.py
+++ code.py
@@ -1,40 +1,29 @@
+class Subject:
+    total_subjects = 0
+    total_types = set()
 
-class people:
-    def __init__(self, people, name, age, mass = None) -> None:
-        print(f"This is a {people} which names {name} which has a {age} years")
-        self.people = people
+    def __init__(self, name, difficult, time):
         self.name = name
-        self.age = age
-        self.mass = 0 if mass==None else mass
-    
-    def speak(self):
-        if self.people == "Teacher":
-            return f"  {self.people} is talking about lesson"
-        elif self.people == "Principal":
-            return f"This {self.people} very important at college"
-        else:
-            return f"This {self.people} is fired :)"
-    
-    def add_one_year(self):
-        self.age += 1
-        return f"This {self.people} made me smarter for 1 year"
-
-first = people("Teacher", "Andriy", 23, 79)
-second = people("Student", "Pavlo", 17, 67)
-third = people(people="Principal", name="Volodimr", age=45, mass=81)
-
-print(f"This {first.people}:", first.speak())
-
-print(f"This {third.people}:", third.speak())
+        self.difficult = difficult
+        self.time = time
+        Subject.total_subjects += 1
+        Subject.total_types.add(name)
 
 
-print(f"{first.people} has {first.age} years")
-print(first.add_one_year())
-print(f"{first.people} now has {first.age} years")
+data = [("math", 4, 15), ("chemistry", 3, 13), ("physics", 4, 14)]
 
-print(f"This {second.people} which names {second.name} has problem with a {first.name} and now they will go to a {third.people} to solve a problem")
 
-print(f"{second.name} is a {second.age} years old")
-print(f"{first.name} is a {first.age} years old")
-print(f"{third.name} is a {third.age} years old")
-print(f"{third.name}is the oldest, and {second.name} is the youngest")
+subjects_list = []
+for name, difficult, time in data:
+    obj = Subject(name, difficult, time)
+    subjects_list.append(obj)
+
+
+print(f"Counter of subjects: {Subject.total_subjects}")
+print(f"Their type: {Subject.total_types}")
+
+
+
+
+
+
