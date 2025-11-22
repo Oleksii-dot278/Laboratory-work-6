e97fc9200afd297e8a31de32b2e675c17cfd372b
Oleksii-dot278 <obukrieiev2023@itcollege.lviv.ua>
2025-11-22 18:14:03 +0100

    Updated

--- code.py
+++ code.py
@@ -1,32 +1,28 @@
-from datetime import datetime
 class People:
-    max_studing = 4
+    total_csv = int(0)
+    total_objects = int(0)
     def __init__(self, profesion, name, age) -> None:
         self.profesion = profesion
         self.name = name
         self.age = age
+        People.total_objects += 1
+
+    @classmethod
+    def from_csv(cls, csv_data:str):
+        cls.total_csv += 1
+        profesion, name, age = csv_data.split(",")
+        return cls(profesion, name, age)
     
-    @property
-    def live_untill(self):
-        return datetime.today().year - self.age + self.max_studing
-    
-    @property
-    def is_adult(self):
-        return self.age >= 18
-    
-    def add_one_year(self):
-        self.age += 1
-        return f"{self.profesion} studing for 1 year"
+    def is_name_capitalized(self):
+        return self.name[0].isupper()
 
 
-first = People("Teacher", "Andriy", 23)
-second = People("Student", "Pavlo", 17)
+first = People.from_csv("Teacher,Andriy,23")
+second = People("Student", "pavlo", 17)
 
-print(f"This {first.profesion} will work until {first.live_untill} year")
-print(f"Is {first.name} an adult? {first.is_adult}")
+print(f"{first.name}, if this name capitalized? {first.is_name_capitalized()}")
+print(f"{second.name} if this name Capitalized? {second.is_name_capitalized()}")
 
-print(f"This {second.profesion} will study until {second.live_untill} year")
-print(f"Is {second.name} an adult? {second.is_adult}")
 
 
 
