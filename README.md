d0becf69c03a9e32cc0fa93b9737f56c305392ed
Oleksii-dot278 <obukrieiev2023@itcollege.lviv.ua>
2025-11-22 17:53:28 +0100

    \updated

--- README.md
+++ README.md
@@ -25,3 +25,7 @@
 ### Завдання №5
 
 ![alt-text](https://github.com/Oleksii-dot278/Laboratory-work-6/blob/main/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202025-11-22%20172637.png)
+
+### Завдання №6
+
+![alt-text](https://github.com/Oleksii-dot278/Laboratory-work-6/blob/main/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202025-11-22%20174905.png)
--- code.py
+++ code.py
@@ -1,26 +1,34 @@
+from datetime import datetime
 class People:
+    max_studing = 4
     def __init__(self, profesion, name, age) -> None:
         self.profesion = profesion
         self.name = name
         self.age = age
     
+    @property
+    def live_untill(self):
+        return datetime.today().year - self.age + self.max_studing
+    
+    @property
+    def is_adult(self):
+        return self.age >= 18
+    
     def add_one_year(self):
         self.age += 1
-        return f" This {self.profesion} has exiriense 1 year"
-
-    @staticmethod
-    def call_people():
-        return "Helpers"
+        return f"{self.profesion} studing for 1 year"
 
 
 first = People("Teacher", "Andriy", 23)
 second = People("Student", "Pavlo", 17)
-third =  People("Principal", "Oksana", 43)
+
+print(f"This {first.profesion} will work until {first.live_untill} year")
+print(f"Is {first.name} an adult? {first.is_adult}")
+
+print(f"This {second.profesion} will study until {second.live_untill} year")
+print(f"Is {second.name} an adult? {second.is_adult}")
 
 
-print(f"Hello {third.name}, and you are a {third.profesion}, if you need a help, you can call {first.call_people()}, or use class metode {People.call_people()} and also, don't forget about {first.name}, he is a profesional {first.profesion}")
-
-print(f"Hello {second.name}, and this is your first day at college, your {first.profesion} is {first.name} be patient and calm")
 
 
 
