a69ae15dfe72b9f4a0ea4ed5532ce4bb22cdfb73
Oleksii-dot278 <obukrieiev2023@itcollege.lviv.ua>
2025-11-23 21:28:54 +0100

    Updated

--- README.md
+++ README.md
@@ -37,3 +37,14 @@
 
 ### Завдання №8
 ![alt-text](https://github.com/Oleksii-dot278/Laboratory-work-6/blob/main/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202025-11-23%20203944.png)
+
+
+### Завдання №9
+![alt-text](https://github.com/Oleksii-dot278/Laboratory-work-6/blob/main/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202025-11-23%20205813.png)
+
+
+### Завдання №10
+![alt-text](https://github.com/Oleksii-dot278/Laboratory-work-6/blob/main/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202025-11-23%20212701.png)
+
+
+### Висновок: Сьогодні я навчився працювати з класами та його основними конструкціями
--- code.py
+++ code.py
@@ -1,36 +1,52 @@
+
 class People:
     def __init__(self, profesion, name, age) -> None:
         self.profesion = profesion
         self.name = name
         self.age = age
-        self._secret_phrase = "private atribute"
-        self.__secret_strong = "    You can't call it from outside"
+
+    def __sub__(self, obj):
+        if isinstance(obj, People):
+            return f"Today {self.name} made smarter {obj.name}"
+        elif isinstance(obj, int):
+            self.age += obj
+            return f"Today {self.name} have a birthday now he has {self.age}"
+        return NotImplementedError
     
-    @property
-    def secret_phrase(self):
-        return f"We got this from property: {self._secret_phrase}"
-    
-    @property
-    def secret_strong(self):
-        return f"We got it from properties: {self.__secret_strong}"
+    def __gt__(self, obj):
+        if isinstance(obj, People):
+            if self.age > obj.age:
+                return f"Старшим є {self.name}"
+            return f"{self.name} не є старшим за {obj.name}"
+        return NotImplementedError
 
-    def _secret_methid(self):
-        return "private metod, don't touch him"
-
-    def __do_not_touch_it(self):
-        return "Can't call from outside"
+    def __len__(self) -> int:
+        print("Ми зайшли в метон len, та знаходимо довжину Імені тваринки")
+        return len(self.name)
     
 
+    def battle(person1, person2):
+     if len(person1.name) > len(person2.name):
+        return f"{person1.name} win bcs longer name"
+     else:
+        return f"{person2.name} win bcs shorter name"
+            
+        
 
-first = People("Principal", "oksana", 43)
-second = People("Teacher", "Andriy", 23)
+first = People("Teacher", "Andriy", 23)
+second = People("Student", "Pavlo", 17)
+third = People("Principal", "Volodimr", 45)
+print(first - second)
+print(first - 1)
+print(first - float(1))
 
-first._secret_phrase = "Changed private atribute"  
+print(first.battle(second))
+
+print(f"{third.name}! You are back, nice to meet you")
+print(f"and also")
+print(third - first)
 
 
-print(second._secret_phrase)
-
-print(second._People__secret_strong)
 
 
 
